220ab6714915155f7aa406818adf9666ab8de42e
phisit77 <kachidoki787@gmail.com>
2023-07-13 15:46:28 +0700

    first app

---
+++ bin/dart_week9.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+import 'number_type.dart';
+
+void main(){
+  numberType();//called methed numberType()
+  multiLineText();
+  valueInput();
+  convertIntToDouble();
+  isMarry();
+  rawString();
+
+}
